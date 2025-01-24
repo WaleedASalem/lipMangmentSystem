c812b5beeacc3884996ddb45498df7db6c0a114b
waleed ayman <124494359+WaleedASalem@users.noreply.github.com>
2025-01-24 10:26:39 +0200

    First commit

---
+++ src/DVD.java
@@ -0,0 +1,24 @@
+public class DVD {
+    private String title;
+    private String director;
+    private int duration;
+    private boolean isAvailable;
+
+
+    public boolean checkAvailability() {
+        //todo to be init
+        return false;
+    }
+
+    public void borrowItem() {
+        //todo to be init
+    }
+
+    public void returnItem() {
+        //todo to be init
+    }
+
+    public void displayDetails() {
+        //todo to be init
+    }
+}
